22b31006144955f51949deea88a95f1e77ba7891
asmiley1 <asmiley1404@gmail.com>
2024-03-23 21:25:53 -0500

    Fully populate the place file

--- logPlace.py
+++ logPlace.py
@@ -2,3 +2,14 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
+root.title("Login")
+
+ttk.Label(root, text= 'Username').place(x=20, y=10)
+ttk.Entry(root).place(x=100, y=10)
+
+ttk.Label(root, text= 'Passcode').place(x=20, y=50)
+ttk.Entry(root).place(x=100, y=50)
+
+ttk.Button(root, text="Login").place(x=100, y=110)
+
+root.mainloop()
