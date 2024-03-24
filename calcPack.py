c1d3de61bc4ede65a7ddc97941fa417e940b39ce
asmiley1 <asmiley1404@gmail.com>
2024-03-23 21:52:44 -0500

    Populated calc file with necessary information, and tested for functionality.

--- calcPack.py
+++ calcPack.py
@@ -2,3 +2,16 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
+
+root.title("Calculator")
+
+ttk.Entry(root, state='disabled').pack()
+
+ttk.Entry(root).pack()
+
+buttons = ['1','2','3','4','5','6','7','8','9','0','+','-','/','*','=']
+
+for btn in buttons:
+    ttk.Button(root, text=btn).pack(side = LEFT)
+
+root.mainloop()
